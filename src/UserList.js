f72ef4d8697f01de2182a07aabcbd8210e782f89
Tom Hinkle <tmhinkle@gmail.com>
2019-11-01 12:31:30 -0400

    ++userlist widgets

---
+++ src/UserList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function User (props) {
+    return (
+        props.menu && 
+            <button className='button user' {...props}>
+              <strong>{props.user.name}</strong> ({props.user.role} - {props.user.purpose})
+            </button>
+        ||
+        <div className='user'>
+          <strong>{props.user.name}</strong> ({props.user.role} - {props.user.purpose})
+        </div>
+    );
+}
+
+function UserList (props) {
+    return (
+            <div>
+              <h2>Visitors</h2>
+              {props.users.map(
+                  (u)=><User user={u} menu={!!props.onSelected} onClick={()=>props.onSelected(u)}/>
+              )}
+            </div>
+    );
+}
+
+export default UserList;
